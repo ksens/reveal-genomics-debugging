19b9f3e64de5faffbc4c7d8a73f8984d80769208
Kriti Sen Sharma <ksen@paradigm4.com>
2018-06-26 17:24:47 +0000

    update server side script to manually specify scidb and shim path
    do not rely on scidb and shim being in path

--- debug.sh
+++ debug.sh
@@ -1,10 +1,11 @@
 #!/bin/bash
 set -x
+SCIDB_VER=18.1
 date
 cat /proc/cpuinfo
 cat /proc/meminfo
-scidb --version
-shim -v
+/opt/scidb/$SCIDB_VER/bin/scidb --version
+/opt/scidb/$SCIDB_VER/bin/shim -v
 cat /opt/scidb/18.1/etc/config.ini
 cat /var/lib/shim/conf
 time iquery -aq "build(<val:double>[i=0:3], i)"
