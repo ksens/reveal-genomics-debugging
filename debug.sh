5c99899ed24d4c024033ac063ecd6255d3fe708c
Kriti Sen Sharma <ksen@paradigm4.com>
2018-06-26 17:35:39 +0000

    update server side script to manually specify iquery path
    do not rely on iquery being in path

--- debug.sh
+++ debug.sh
@@ -6,6 +6,6 @@ cat /proc/cpuinfo
 cat /proc/meminfo
 /opt/scidb/$SCIDB_VER/bin/scidb --version
 /opt/scidb/$SCIDB_VER/bin/shim -v
-cat /opt/scidb/18.1/etc/config.ini
+cat /opt/scidb/$SCIDB_VER/etc/config.ini
 cat /var/lib/shim/conf
-time iquery -aq "build(<val:double>[i=0:3], i)"
+time /opt/scidb/$SCIDB_VER/bin/iquery -aq "build(<val:double>[i=0:3], i)"
