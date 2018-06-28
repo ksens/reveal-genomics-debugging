6fd03ace8a75320d8f51d2dd80f45c0986875e96
Kriti Sen Sharma <ksen@paradigm4.com>
2018-06-28 15:21:40 -0400

    add bottoms up search query example

---
+++ new_queries.R
@@ -0,0 +1,29 @@
+# find all samples in all dataset that has gene “TGFB1” expression value > 100
+rm(list=ls())
+library(scidb4gh)
+con = gh_connect2()
+db = con$db
+
+#### USER PARAMETERS ####
+lo_thresh = 2
+gene_symbol = c('IGLC4', 'AC034198.7')
+
+rnaq_arr = scidb4gh:::full_arrayname(.ghEnv$meta$arrRnaquantification)
+ftr_arr  = scidb4gh:::full_arrayname(.ghEnv$meta$arrFeature)
+
+feature_subquery = paste0("gene_symbol='", gene_symbol, "'", collapse = " OR ")
+selection_q = paste0(
+          "cross_join(
+              filter(", rnaq_arr, ", value > ", lo_thresh, ") as X, ",
+                     "filter(", ftr_arr, ", ", feature_subquery, ") as Y, ",
+                     "X.feature_id, Y.feature_id)")
+res = 
+  iquery(con$db, 
+       paste0(
+         "aggregate(", selection_q, ", count(*), biosample_id)"),
+       return = T)
+   
+biosample_ids = res$biosample_id
+cat("The biosample ids that match user specified criteria are: ", pretty_print(biosample_ids), 
+    "\nRetrieving the phenotype metadata for these samples\n")
+print(get_biosamples(biosample_id = biosample_ids, con = con))
