3a78e562ad478dc492cdd64a323030e0ef2c1e83
Kriti Sen Sharma <ksen@paradigm4.com>
2018-06-29 09:22:08 -0400

    example for search across studies

--- new_queries.R
+++ new_queries.R
@@ -27,3 +27,30 @@ biosample_ids = res$biosample_id
 cat("The biosample ids that match user specified criteria are: ", pretty_print(biosample_ids), 
     "\nRetrieving the phenotype metadata for these samples\n")
 print(get_biosamples(biosample_id = biosample_ids, con = con))
+
+#### Query 2 ####
+
+# search for the measurement data without specifying the measurement set. 
+# ... limit the total number of rows retrieved, 
+# or only return the first N rows.
+
+#### USER PARAMETERS ####
+lo_thresh = 2
+gene_symbol = c('IGLC4', 'AC034198.7')
+N = 1000
+
+
+feature_subquery = paste0("gene_symbol='", gene_symbol, "'", collapse = " OR ")
+
+# then
+selection_q = paste0(
+  "cross_join(", rnaq_arr, " as X, ",
+  "filter(", ftr_arr, ", ", feature_subquery, ") as Y, ",
+  "X.feature_id, Y.feature_id)")
+limit_q = paste0("limit(", selection_q, ",", N, ")")
+
+res = 
+  iquery(con$db, 
+         limit_q,
+         return = T)
+print(res)
