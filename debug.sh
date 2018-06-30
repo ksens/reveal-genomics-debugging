7f9a1ecb1335c063e0580819f3cd0a1c68168345
Kriti Sen Sharma <ksen@paradigm4.com>
2018-06-30 15:49:11 -0400

    add command to check for hdd vs ssd

--- debug.sh
+++ debug.sh
@@ -8,4 +8,11 @@ cat /proc/meminfo
 /opt/scidb/$SCIDB_VER/bin/shim -v
 cat /opt/scidb/$SCIDB_VER/etc/config.ini
 cat /var/lib/shim/conf
+
+echo "Time a simple build query"
 time /opt/scidb/$SCIDB_VER/bin/iquery -aq "build(<val:double>[i=0:3], i)"
+
+echo "Two methods for checking if data-disks are SSD-s or HDD: 1 for hard disks and 0 for a SSD."
+# http://fibrevillage.com/storage/599-how-to-tell-if-a-disk-is-ssd-or-hdisk-on-linux
+lsblk -d -o name,hctl,rota
+for diskname in `ls /sys/block/`; do echo $diskname; cat /sys/block/$diskname/queue/rotational; done
